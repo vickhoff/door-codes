859e84991c9bc0cc9710ce0a2c0fbab66c5b02e9
vickhoff <dvickhoff@gmail.com>
2026-01-22 13:59:57 +0100

    Remove services and do all in controllers

--- controllers/items.js
+++ controllers/items.js
@@ -1,10 +1,10 @@
-import { addItem, listItems, removeItem } from "../services/items.js"
+import Item from "../models/item.js"
 
 const createItem = async (req, res, next) => {
     try {
         const { name, address, code } = req.body;
-        const data = await addItem({ name, address, code });
-        res.status(201).json(data);
+        const data = await Item.create({ name, address, code });
+        res.status(200).json(data);
     } catch (error) {
         next(error);
     }
@@ -12,7 +12,7 @@ const createItem = async (req, res, next) => {
 
 const getItems = async (req, res, next) => {
     try {
-        const data = await listItems();
+        const data = await Item.find();
         res.status(200).json(data);
     } catch (error) {
         next(error);
@@ -21,10 +21,18 @@ const getItems = async (req, res, next) => {
 
 const deleteItem = async (req, res, next) => {
     try {
-        await removeItem(req.params.id);
+        const deleteResult = await Item.findByIdAndDelete(req.params.id);
+
+        if (!deleteResult) {
+            const error = new Error("Item wasn't found.");
+            error.status = 404;
+            throw error;
+        }
+
         res.status(200).json({
             message: "Item was deleted successfully"
         });
+
     } catch (error) {
         next(error);
     }
