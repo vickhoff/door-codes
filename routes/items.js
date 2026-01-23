921ad71a7fc7919f9ba90f068a6eb7ef15fc78bf
vickhoff <dvickhoff@gmail.com>
2026-01-23 14:36:32 +0100

    Handle more errors

--- routes/items.js
+++ routes/items.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createItem, deleteItem, getItems } from "../controllers/items.js";
+import { createItem, deleteItem, getItems, getItem } from "../controllers/items.js";
 
 const router = express.Router();
 
 router.post("/add", createItem);
 router.get("/", getItems);
+router.get("/:id", getItem);
 router.delete("/delete/:id", deleteItem)
 
 export default router;
