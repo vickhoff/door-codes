d4043d52a12059c6499ea294edc9cf9467d5cf0b
vickhoff <dvickhoff@gmail.com>
2026-01-28 14:32:02 +0100

    Authentication on endpoint get one item

--- controllers/items.js
+++ controllers/items.js
@@ -4,7 +4,12 @@ const createItem = async (req, res, next) => {
     try {
         const { name, address, code } = req.body;
         const userId = req.user._id;
-        const data = await Item.create({ name, address, code, userId: userId });
+        const data = await Item.create({ 
+            name, 
+            address, 
+            code, 
+            userId: userId 
+        });
         res.status(200).json(data);
     } catch (error) {
         if (error.name === "ValidationError") {
@@ -31,6 +36,45 @@ const getItem = async (req, res, next) => {
         const data = await Item.findById(req.params.id);
 
         res.status(200).json(data);
+    } catch (error) {
+        if (error.name === "CastError") {
+            return res.status(404).json({
+                error: "Item wasn't found"
+            });
+        }
+        next(error);
+    }
+}
+
+const updateItem = async (req, res, next) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        const userId = req.user._id;
+        const itemUserId = item.userId
+        
+        //If UserId and items UserId doesnt match
+        if (String(userId) != String(itemUserId)) {
+            const error = new Error("Not authorized.");
+            error.status = 403;
+            throw error;
+        }
+
+        const updateData = await Item.findByIdAndUpdate(req.params.id, {
+            name, 
+            address, 
+            code 
+        });
+
+        if (!updateData) {
+            const error = new Error("Item wasn't found.");
+            error.status = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            message: "Item was updated successfully"
+        });
+
     } catch (error) {
         if (error.name === "CastError") {
             return res.status(404).json({
@@ -76,4 +120,4 @@ const deleteItem = async (req, res, next) => {
     }
 }
 
-export { createItem, getItems, getItem, deleteItem }
+export { createItem, getItems, getItem, updateItem, deleteItem }
--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { createItem, deleteItem, getItems, getItem } from "../controllers/items.js";
+import { createItem, deleteItem, getItems, getItem, updateItem } from "../controllers/items.js";
 import { authenticate } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/add", authenticate, createItem);
 router.get("/", authenticate, getItems);
-router.get("/:id", getItem);
+router.patch("/update/:id", authenticate, updateItem)
+router.get("/:id", authenticate, getItem);
 router.delete("/delete/:id", authenticate, deleteItem)
 
 export default router;
