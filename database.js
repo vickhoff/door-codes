5670717d3d44a6261707e5fbb546e4b9b9abd6b6
vickhoff <dvickhoff@gmail.com>
2026-03-09 13:56:59 +0100

    Update database.js

--- database.js
+++ database.js
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
+    if (mongoose.connection.readyState >= 1) {
+        return; // Already connected
+    }
     await mongoose.connect(process.env.MONGO_URI, {
         dbName: "door-codes"
-    })
-    console.log("MongoDB Connected");
-}
+    });
+};
 
 
 
