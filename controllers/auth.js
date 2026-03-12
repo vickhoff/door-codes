02c837975e0ff491bd9151b212d97ba41d497cda
vickhoff <dvickhoff@gmail.com>
2026-03-12 22:34:24 +0100

    change back to before testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,7 +34,7 @@ const login = async (req, res, next) => {
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(404).json({
-                message: "User not foundrrrr"
+                message: "User not found"
             });
         }
 
