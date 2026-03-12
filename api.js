9855c4afc0a86cdae65ee9ade7ed5bc8f79e98e1
vickhoff <dvickhoff@gmail.com>
2026-03-12 10:48:29 +0100

    Add cors

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
 import { connectToDatabase } from "./database.js";
 import express from "express";
+import cors from "cors"
 
 import itemsRouter from "./routes/items.js";
 import usersRouter from "./routes/users.js";
 import authRouter from "./routes/auth.js";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 const appPort = 3000;
 
