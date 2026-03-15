57887b6d8e098cb2207a873d27c1fdd368e35782
vickhoff <dvickhoff@gmail.com>
2026-03-15 12:24:31 +0100

    Saved api.js

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ import authRouter from "./routes/auth.js";
 const app = express();
 app.use(cors())
 app.use(express.json());
-app.use(cookieParser)
+app.use(cookieParser())
 const appPort = 3000;
 
 // Ensure DB connection before handling requests (cached in database.js)
